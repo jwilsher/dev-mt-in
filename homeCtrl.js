002eeb6588e50c9641ce813bff0bca212093fadb
Jaclyn Cowley <jaclyncowley11@Jaclyns-MacBook-Pro-2.local>
2016-02-23 18:47:20 -0700

    Controller

---
+++ homeCtrl.js
@@ -0,0 +1,52 @@
+angular.module('devMtIn')
+.controller('homeCtrl', function($scope, profileService) {
+    //$scope.myProfile = profileService.checkForProfile();
+    
+    $scope.checkForProfile = function() {
+        var profileId = JSON.parse(localStorage.getItem('profileId'));
+
+        if (profileId) {
+            profileService.checkForProfile(profileId.profileId)
+            .then(function(profile) {
+            $scope.myProfile = profile.data;
+            })
+            .catch(function(err) {
+            console.error(err);
+            });
+        }
+    }
+    
+    $scope.checkForProfile();
+    
+    $scope.sortOptions = [{
+        display: 'Ascending'
+      , value: false
+      },
+      {
+        display: 'Descending'
+      , value: true
+      }
+];
+    profileService.serviceTest();
+    
+    $scope.editing = false;
+    
+    $scope.saveProfile = function(profile) {
+        profileService.saveProfile(profile);
+        $scope.editing = false;
+    }
+    
+    $scope.deleteProfile = function(profile) {
+        profileService.deleteProfile()
+        .then(function(deletedProfile) {
+            localStorage.removeItem('profileId');
+            $scope.myProfile = {};
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+        
+        //profileService.deleteProfile(profile);
+        //$scope.myProfile = profileService.checkForProfile();
+    }
+});
